7b49917fa828ae698dece98c9dd4f909566b9e67
Matt James <matt.james@secondstreet.com>
2020-08-27 09:08:53 -0500

    :lipstick: clean up layout about page

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,22 +1,24 @@
-<style>
-  figure {
-    float: right;
-    margin: -3em -7em 2em 2em;
-    max-width: 200px;
-  }
-
+<style type="text/scss">
   img {
     border-radius: 8px;
-    float: right;
-    margin: -3em -7em 2em 2em;
-    max-width: 400px;
+    grid-column: 2/3;
   }
-
+  .content-wrapper {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 1em;
+  }
+  .content-wrapper__text-block {
+    grid-column: 1/2;
+    grid-row: 1/2;
+  }
   @media (max-width: 1020px) {
-    img {
-      float: none;
-      margin: 0 auto 2em;
-    }
+   .content-wrapper {
+     grid-template-columns: 1fr;
+   }
+   img {
+     grid-column: 1/2;
+   }
   }
 </style>
 
@@ -27,10 +29,13 @@
 <div class="container">
   <h1>About Me</h1>
   <h2>Hi There.</h2>
-        <img src="/profile-pic.jpg" alt="Matt James">
-        <p>My name is Matt James and I make things on the web.</p>
-        <p>
-          I've also been know to take photographs, abuse the guitar and play in
-          the woods on bikes.
-        </p>
+  <div class="content-wrapper">
+    <img src="/profile-pic.jpg" alt="Matt James">
+    <div class="content-wrapper__text-block">
+      <p>My name is Matt James and I make things on the web.</p>
+      <p>
+        I've also been know to take photographs, abuse the guitar and play in the woods on bikes.
+      </p>
+    </div>
+  </div>
 </div>
